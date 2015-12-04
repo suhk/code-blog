5951672d744b0c2779bbb63e95fa895dd504c711
Natalie Chow <cychownat@gmail.com>
2015-12-04 13:47:33 -0800

    Display stringify JSON on page with preview

--- scripts/new.js
+++ scripts/new.js
@@ -3,21 +3,22 @@ $('#article-preview').on('click', function(event) {
 
   var prop = {};
 
+  // read values from form
   prop.title = $('#article-title').val();
   prop.author = $('#article-author').val();
   prop.authorUrl = $('#article-author-url').val();
   prop.category = $('#article-category').val();
   prop.publishedOn = new Date();
 
-  $articleBody = $('#article-body').val();
-  
+  $articleBody = $('#article-body').val();  
   prop.body = marked($articleBody);
 
-  console.log(prop);
-
+  // display preview on page
   var newArticle = new Article(prop);
-  console.log(newArticle);
   newArticle.toHTML();
+
+  // write JSON string
+  $('#article-json').val(JSON.stringify(prop));
   
 });
 
