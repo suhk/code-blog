c8f0c51dcd72e4afd3881144478f5378b9847463
suhk <kennethgenesuh@gmail.com>
2015-12-07 09:36:55 -0800

    Removed outdated function

--- scripts/article.js
+++ scripts/article.js
@@ -11,30 +11,6 @@ var Article = function(props) {
     this.daysElapsed = Math.round((today - date) / 1000 / 60 / 60 / 24);
 }
 
-// Turns an article object into HTML code
-Article.prototype.toHTML2 = function () {
-    var $art = $('article:first').clone();
-
-    $art.find('h1').append(this.title);
-
-    $art.find('h3').attr('class', this.author.replace(/\s/g, ''));
-    $art.find('h3').prepend("Written by");
-
-    $art.find('.auth').append(this.author);
-    $art.find('.auth').attr('href', this.authorUrl)
-
-    $art.find('h3').append(" about " + this.daysElapsed + " days ago");
-
-    $art.find('h5').append("Category: " + this.category);
-    $art.find('h5').attr('class', this.category);
-
-    $art.find('.body').append(this.body);
-
-    $art.find('.expand').append("Read More >");
-
-    $('main').append($art);
-}
-
 // Takes the handlebars template, fill it, and add it to the web page
 Article.prototype.toHTML = function (selector) {
     var data = {
