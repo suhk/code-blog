81b9d626549fb77ad532b177f0062de705ca4ec0
suhk <kennethgenesuh@gmail.com>
2015-12-08 10:24:56 -0800

    Cleaned up index.js with named functions

--- scripts/index.js
+++ scripts/index.js
@@ -4,27 +4,35 @@
 $(function() {
     var blog = new Blog(); // Blog object
 
-    $.get("scripts/articleTemplate", function(data) {
+    // Grab the article template and make a handlebars function
+    blog.compileTemplate = function(data) {
         Article.prototype.compiledTemplate = Handlebars.compile(data);
-    }).done(function() {
-        $.ajax({
-            url: 'scripts/blogArticles.json',
-            success: function(data, textStatus, xhr) {
-                if(localStorage.getItem("data") == null || xhr.getResponseHeader('ETag') != localStorage.getItem("ETag")) {
-                    localStorage.setItem("ETag", xhr.getResponseHeader('ETag'));
-                    $.getJSON('scripts/blogArticles.json', function(data) {
-                        localStorage.setItem("data", JSON.stringify(data));
-                        blog.init(data); // Fill the blog
-                        blog.addEvents(); // Add handlers to blog
-                    });
-                } else {
-                    blog.init(JSON.parse(localStorage.getItem("data")));
-                    blog.addEvents();
-                }
-            }
-        });
+    };
 
-    });
+    blog.pullJSON = function(data, textStatus, xhr) {
+        // Check localStorage to see if data has changed
+        // If so, then pull from json file and update localStorage
+        if(localStorage.getItem("data") == null || xhr.getResponseHeader('ETag') != localStorage.getItem("ETag")) {
+            localStorage.setItem("ETag", xhr.getResponseHeader('ETag'));
+
+            // Grab the JSON using ajax
+            $.getJSON('scripts/blogArticles.json', function(data) {
+                localStorage.setItem("data", JSON.stringify(data));
+                blog.init(data); // Fill the blog
+                blog.addEvents(); // Add handlers to blog
+            });
+        } else {
+            // Otherwise, just pull articles from localStorage
+            blog.init(JSON.parse(localStorage.getItem("data"))); // Fill the blog
+            blog.addEvents(); // Add handlers to blog
+        }
+    };
+
+    $.get("scripts/articleTemplate", blog.compileTemplate)
+        .done($.ajax({
+            url: 'scripts/blogArticles.json',
+            success: blog.pullJSON
+        }));
 
 
 
