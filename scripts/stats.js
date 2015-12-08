db55888c89f6a523de4ac4c6174f4faea1cf4c61
suhk <kennethgenesuh@gmail.com>
2015-12-08 15:20:27 -0800

    Done with use cases

--- scripts/stats.js
+++ scripts/stats.js
@@ -42,9 +42,19 @@ stats.summer = function(counter, a) {
     return counter + a;
 };
 
-stats.wlAverager = function(art) {
-    return 0;
-}
+stats.strLength = function(str) {
+    return str.length;
+};
+
+stats.wlCounter = function(art) {
+    var body = stats.removeMd(art.markdown);
+    return body.split(new RegExp(' ', 'g')).map(stats.strLength).reduce(stats.summer, 0);
+};
+
+stats.wlCounterWithAuthor = function(art) {
+    var body = stats.removeMd(art.markdown);
+    return {wl: body.split(new RegExp(' ', 'g')).map(stats.strLength).reduce(stats.summer, 0), author: art.author, words: body.split(new RegExp(' ', 'g')).length};
+};
 
 stats.countArticles = function(articles) {
     return articles.length;
@@ -60,14 +70,46 @@ stats.countWords = function(articles) {
 };
 
 stats.avgWL = function(articles) {
-    return articles.map(stats.wlAverager).reduce(stats.summer, 0) / articles.length;
-}
+    return articles.map(stats.wlCounter).reduce(stats.summer, 0) / stats.wordCount;
+};
+
+stats.getAuthors = function(art) {
+    return art.author;
+};
+
+stats.someFunction = function(counter, a) {
+    if(counter[0].indexOf(a.author) < 0) {
+        counter[0].push(a.author);
+        counter[1].push([a.wl, a.words]);
+    } else {
+        counter[1][counter[0].indexOf(a.author)][0] += a.wl;
+        counter[1][counter[0].indexOf(a.author)][1] += a.words;
+    }
+    return counter;
+};
+
+stats.someOtherFunction = function(counter, a) {
+    counter.push(a[0] / a[1]);
+    return counter;
+};
+
+stats.avgWLAuthor = function(articles) {
+    var wls = articles.map(stats.wlCounterWithAuthor);
+    var arr = wls.reduce(stats.someFunction, [[], []]);
+    return [arr[0], arr[1].reduce(stats.someOtherFunction, [])];
+};
 
 $(function() {
     $.getJSON('scripts/blogArticles.json', function(data) {
+        stats.wordCount = stats.countWords(data);
         $('#numArticles').append('Number of Articles: ' + stats.countArticles(data));
         $('#numAuthors').append('Number of Authors: ' + stats.countAuthors(data));
-        $('#numWords').append('Number of Words: ' + stats.countWords(data));
-        $('#avgWL').append('Number of Words Per: ' + stats.avgWL(data));
+        $('#numWords').append('Number of Words: ' + stats.wordCount);
+        $('#avgWL').append('Average Word Length: ' + stats.avgWL(data));
+        $('#avgWLAuthor').prepend('Average Word Length Per Author:');
+        var wlAuthorStats = stats.avgWLAuthor(data);
+        for(var i = 0; i < wlAuthorStats[0].length; i++) {
+            $('#authList').append('<li>' + wlAuthorStats[0][i] + ': ' + wlAuthorStats[1][i] + '</li>');
+        }
     });
 });
