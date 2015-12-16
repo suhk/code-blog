7d68d18478b7ca721b40e9e939b826a9fcfaa3c7
suhk <kennethgenesuh@gmail.com>
2015-12-16 02:49:57 -0800

    eslint

--- scripts/article.js
+++ scripts/article.js
@@ -1,36 +1,36 @@
 var Article = function(props) {
-    this.author = props.author;
-    this.authorUrl = props.authorUrl;
-    this.category = props.category;
-    this.title = props.title;
-    this.body = props.body;
-    this.publishedOn = props.publishedOn;
+  this.author = props.author;
+  this.authorUrl = props.authorUrl;
+  this.category = props.category;
+  this.title = props.title;
+  this.body = props.body;
+  this.publishedOn = props.publishedOn;
 
-    var date = new Date(this.publishedOn);
-    var today = new Date();
-    this.daysElapsed = Math.round((today - date) / 1000 / 60 / 60 / 24);
-}
+  var date = new Date(this.publishedOn);
+  var today = new Date();
+  this.daysElapsed = Math.round((today - date) / 1000 / 60 / 60 / 24);
+};
 
 // Turns an article object into HTML code
 Article.prototype.toHTML = function () {
-    var $art = $('article:first').clone();
+  var $art = $('article:first').clone();
 
-    $art.find('h1').append(this.title);
+  $art.find('h1').append(this.title);
 
-    $art.find('h3').attr('class', this.author.replace(/\s/g, ''));
-    $art.find('h3').prepend("Written by");
+  $art.find('h3').attr('class', this.author.replace(/\s/g, ''));
+  $art.find('h3').prepend('Written by');
 
-    $art.find('.auth').append(this.author);
-    $art.find('.auth').attr('href', this.authorUrl)
+  $art.find('.auth').append(this.author);
+  $art.find('.auth').attr('href', this.authorUrl);
 
-    $art.find('h3').append(" about " + this.daysElapsed + " days ago");
+  $art.find('h3').append(' about ' + this.daysElapsed + ' days ago');
 
-    $art.find('h5').append("Category: " + this.category);
-    $art.find('h5').attr('class', this.category);
+  $art.find('h5').append('Category: ' + this.category);
+  $art.find('h5').attr('class', this.category);
 
-    $art.find('.body').append(this.body);
+  $art.find('.body').append(this.body);
 
-    $art.find('.expand').append("Read More >");
+  $art.find('.expand').append('Read More >');
 
-    $('main').append($art);
-}
+  $('main').append($art);
+};
