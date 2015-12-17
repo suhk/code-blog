d8259134f054a7ddf052a5db942134039c3fdb9a
suhk <kennethgenesuh@gmail.com>
2015-12-17 09:01:06 -0800

    linter

--- scripts/articleController.js
+++ scripts/articleController.js
@@ -3,66 +3,66 @@ var articleController = {};
 var blog = new Blog(); // Blog object
 
 articleController.callDB = function(data, msg, xhr) {
-    webDB.init();
-    webDB.setupTables();
+  webDB.init();
+  webDB.setupTables();
 
-    var eTag = xhr.getResponseHeader('eTag');
-    if (typeof localStorage.articlesEtag == 'undefined' || localStorage.articlesEtag != eTag) {
-        console.log('cache miss!');
-        localStorage.articlesEtag = eTag;
+  var eTag = xhr.getResponseHeader('eTag');
+  if (typeof localStorage.articlesEtag == 'undefined' || localStorage.articlesEtag != eTag) {
+    console.log('cache miss!');
+    localStorage.articlesEtag = eTag;
 
-        // Remove all prior articles from the DB, and from blog:
-        blog.articles = [];
-        webDB.execute('delete from articles;', articleController.getJSON);
+    // Remove all prior articles from the DB, and from blog:
+    blog.articles = [];
+    webDB.execute('delete from articles;', articleController.getJSON);
 
-    } else {
-        console.log('cache hit!');
-        webDB.execute('select * from articles;', articleController.dbFetch);
+  } else {
+    console.log('cache hit!');
+    webDB.execute('select * from articles;', articleController.dbFetch);
 
-    }
+  }
 
 };
 
 articleController.dbFetch = function(result) {
-    result.forEach(function(e) {
-        blog.articles.push(new Article(e));
-    });
-    blog.init();
-    blog.addEvents();
+  result.forEach(function(e) {
+    blog.articles.push(new Article(e));
+  });
+  blog.init();
+  blog.addEvents();
 
 };
 
 articleController.updateDB = function(data) {
-    // Iterate over new article JSON:
-    data.forEach(function(item) {
-        console.log(item);
-        // Instantiate an article based on item from JSON:
-        var article = new Article(item);
+  // Iterate over new article JSON:
+  data.forEach(function(item) {
+    console.log(item);
+    // Instantiate an article based on item from JSON:
+    var article = new Article(item);
 
-        // Add the article to blog.articles
-        blog.articles.push(article);
+    // Add the article to blog.articles
+    blog.articles.push(article);
 
-        // Cache the article in DB
-        webDB.insertRecord(article);
-    });
+    // Cache the article in DB
+    webDB.insertRecord(article);
+  });
 
-    blog.init();
-    blog.addEvents();
+  blog.init();
+  blog.addEvents();
 
 };
 
 articleController.getJSON = function() {
-    $.getJSON('/scripts/blogArticles.json', articleController.updateDB);
+  $.getJSON('/scripts/blogArticles.json', articleController.updateDB);
 };
 
 articleController.index = function() {
-    $.ajaxPrefilter(function(options) {
-        options.async = true;
-    });
-    $.get('/scripts/articleTemplate', blog.compileTemplate)
-        .done($.ajax({
-            type: 'HEAD',
-            url: '/scripts/blogArticles.json',
-            success: articleController.callDB
-        }));
+  $.ajaxPrefilter(function(options) {
+    options.async = true;
+  });
+  $.get('/scripts/articleTemplate', blog.compileTemplate)
+  .done($.ajax({
+    type: 'HEAD',
+    url: '/scripts/blogArticles.json',
+    success: articleController.callDB
+  }));
 };
--- scripts/blog.js
+++ scripts/blog.js
@@ -1,122 +1,122 @@
 var Blog = function() {
-    this.articles = []; // Instantiate an array of articles
-    this.cat = 'None'; // The current selected category
-    this.auth = 'None'; // The current selected author
+  this.articles = []; // Instantiate an array of articles
+  this.cat = 'None'; // The current selected category
+  this.auth = 'None'; // The current selected author
 };
 
 // Grab the article template and make a handlebars function
 Blog.prototype.compileTemplate = function(data) {
-    Article.prototype.compiledTemplate = Handlebars.compile(data);
+  Article.prototype.compiledTemplate = Handlebars.compile(data);
 };
 
 Blog.prototype.init = function() {
-    // Sort the articles by date (recent first)
-    this.sortData();
-    // HTMLizes the articles
-    for(i = 0; i < this.articles.length; i++) {
+  // Sort the articles by date (recent first)
+  this.sortData();
+  // HTMLizes the articles
+  for(i = 0; i < this.articles.length; i++) {
 
-        this.articles[i].toHTML('main');
+    this.articles[i].toHTML('main');
+  }
+
+  $('#about').hide();
+
+  // Hide everything but the first paragraph
+  $('article p:not(:first-child)').hide();
+
+  // Get the list of categories and authors for sorting and put them into dropdowns
+  var cats = [];
+  var auths = [];
+  for(i = 0; i < this.articles.length; i++) {
+    // If it is a new category, add it to the list
+    if(cats.indexOf(this.articles[i].category) < 0) {
+      cats.push(this.articles[i].category);
+
+      var $op = $('option:first').clone();
+      $op.text(this.articles[i].category);
+      $('#category').append($op);
     }
 
-    $('#about').hide();
+    // If it is a new author, add it to the list
+    if(auths.indexOf(this.articles[i].author) < 0) {
+      auths.push(this.articles[i].author);
 
-    // Hide everything but the first paragraph
-    $('article p:not(:first-child)').hide();
-
-    // Get the list of categories and authors for sorting and put them into dropdowns
-    var cats = [];
-    var auths = [];
-    for(i = 0; i < this.articles.length; i++) {
-        // If it is a new category, add it to the list
-        if(cats.indexOf(this.articles[i].category) < 0) {
-            cats.push(this.articles[i].category);
-
-            var $op = $('option:first').clone();
-            $op.text(this.articles[i].category);
-            $('#category').append($op);
-        }
-
-        // If it is a new author, add it to the list
-        if(auths.indexOf(this.articles[i].author) < 0) {
-            auths.push(this.articles[i].author);
-
-            var $op2 = $('option:first').clone();
-            $op2.text(this.articles[i].author);
-            $('#author').append($op2);
-        }
+      var $op2 = $('option:first').clone();
+      $op2.text(this.articles[i].author);
+      $('#author').append($op2);
     }
+  }
 };
 
 Blog.prototype.getParameterByKey = function(key) {
-    var match = RegExp('[?&]' + key + '=([^&]*)').exec(window.location.search);
-    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+  var match = RegExp('[?&]' + key + '=([^&]*)').exec(window.location.search);
+  return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
 };
 
 Blog.prototype.isAdmin = function() {
-    var admin = blog.getParameterByKey('admin');
-    if (admin == 'true') {
-        return true;
-    }
-    return false;
+  var admin = blog.getParameterByKey('admin');
+  if (admin == 'true') {
+    return true;
+  }
+  return false;
 };
 
 Blog.prototype.addEvents = function() {
-    // Add click event for 'read more'
-    $('main').on('click', '.expand', function(e) {
-        e.preventDefault();
+  // Add click event for 'read more'
+  $('main').on('click', '.expand', function(e) {
+    e.preventDefault();
 
-        $(this).parent().find('p').fadeIn(500);
-        $(this).fadeOut();
-    });
+    $(this).parent().find('p').fadeIn(500);
+    $(this).fadeOut();
+  });
 
-    // Add click events for nav bar
-    $('.tab').on('click', function(e) {
-        e.preventDefault();
-        $('section').hide();
-        $('#' + $(this).data('content')).fadeIn(500);
-    });
+  // Add click events for nav bar
+  $('.tab').on('click', function(e) {
+    e.preventDefault();
+    $('section').hide();
+    $('#' + $(this).data('content')).fadeIn(500);
+  });
 
-    // Add event handler for category search
-    $('#category').on('change', function() {
-        $('#author').val('null'); // Resets the author filter
-        this.cat = $('#category option:selected').text(); // Set the category filter
+  // Add event handler for category search
+  $('#category').on('change', function() {
+    $('#author').val('null'); // Resets the author filter
+    this.cat = $('#category option:selected').text(); // Set the category filter
 
-        // If you select 'None', show all articles
-        // Otherwise, show filtered articles
-        if(this.cat == 'None') {
-            $('article').fadeIn(500);
-        } else {
-            $('article').hide();
-            $('.' + $('#category option:selected').text().replace(/\s/g, '')).parent().fadeIn(500);
-        }
-    });
+    // If you select 'None', show all articles
+    // Otherwise, show filtered articles
+    if(this.cat == 'None') {
+      $('article').fadeIn(500);
+    } else {
+      $('article').hide();
+      $('.' + $('#category option:selected').text().replace(/\s/g, '')).parent().fadeIn(500);
+    }
+  });
 
-    // Add event handler for author search
-    $('#author').on('change', function() {
-        $('#category').val('null'); // Resets the author filter
-        this.auth = $('#author option:selected').text(); // Set the author filter
+  // Add event handler for author search
+  $('#author').on('change', function() {
+    $('#category').val('null'); // Resets the author filter
+    this.auth = $('#author option:selected').text(); // Set the author filter
 
-        // If you select 'None', show all articles
-        // Otherwise, show filtered articles
-        if(this.auth == 'None') {
-            $('article').fadeIn(500);
-        } else {
-            $('article').hide();
-            $('.' + $('#author option:selected').text().replace(/\s/g, '')).parent().fadeIn(500);
-        }
-    });
+    // If you select 'None', show all articles
+    // Otherwise, show filtered articles
+    if(this.auth == 'None') {
+      $('article').fadeIn(500);
+    } else {
+      $('article').hide();
+      $('.' + $('#author option:selected').text().replace(/\s/g, '')).parent().fadeIn(500);
+    }
+  });
 };
 
 // CompareByDate function for sorting the articles
 // Taken from Microsoft website
 Blog.prototype.sortData = function() {
-    this.articles.sort(
-        function compareByDate(a, b) {
-            if (a.daysElapsed < b.daysElapsed)
-                return -1;
-            if (a.daysElapsed > b.daysElapsed)
-                return 1;
-            return 0;
-        }
-    );
+  this.articles.sort(
+    function compareByDate(a, b) {
+      if (a.daysElapsed < b.daysElapsed)
+        return -1;
+      if (a.daysElapsed > b.daysElapsed)
+        return 1;
+      return 0;
+    }
+  );
 };
